e80442331d84296469f0cd3112822474e8808fd6
Богдан Лешко <1killcube1@gmail.com>
2021-04-06 23:10:48 +0300

    initializing project, signup page+firebase connection

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_twitter_clone/ui/theme/theme.dart';
+import 'helper/routes.dart';
+import 'state/appState.dart';
+import 'package:provider/provider.dart';
+import 'state/authState.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<AppState>(create: (_) => AppState()),
+        ChangeNotifierProvider<AuthState>(create: (_) => AuthState()),
+
+      ],
+      child: MaterialApp(
+        title: 'Fwitter',
+        theme: AppTheme.apptheme.copyWith(
+          textTheme: GoogleFonts.muliTextTheme(
+            Theme.of(context).textTheme,
+          ),
+        ),
+        debugShowCheckedModeBanner: false,
+        routes: Routes.route(),
+        onGenerateRoute: (settings) => Routes.onGenerateRoute(settings),
+        onUnknownRoute: (settings) => Routes.onUnknownRoute(settings),
+        initialRoute: "SplashPage",
+      ),
+    );
+  }
+}
